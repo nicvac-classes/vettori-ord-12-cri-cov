52e3cbea70c3db9c8ab7a0c512792c2474e38c9e
cri-cov <cristian.covato@marconibari.edu.it>
2025-03-19 17:46:59 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "numero di cifre da inserire?" << endl;
+    cin >> n;
+    i = 0;
+    double r[n];
+
+    while (i < n) {
+        r[i] = 0 + rand() % (10 * n);
+        i = i + 1;
+    }
+    int j, t;
+
+    i = 0;
+    t = 0;
+    while (i < n) {
+        j = 0;
+        while (j < n - 1) {
+            if (r[j] > r[j + 1]) {
+                t = (int) r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = t;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << r[i] << endl;
+        i = i + 1;
+    }
+   
